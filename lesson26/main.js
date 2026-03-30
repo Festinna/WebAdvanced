3873da2ee851feb686fab89c46d9aab73d20f029
Festinna <festinakrasniqi09@gmail.com>
2026-03-30 16:15:02 +0200

    lesson27

---
+++ lesson26/main.js
@@ -0,0 +1,86 @@
+console.log("Goodbye");
+
+//var x = 5;
+
+var a = 50 + 50;
+console.log(a);
+
+var b = 20;
+var c = 30;
+var d = b + c;
+console.log(d);
+
+var e = 40;
+var f = 10;
+var g = e - f;
+console.log(g);
+
+var h = 10;
+var i = 4;
+var j = h % i;
+console.log(j);
+
+var k = 99;
+k++;
+console.log(k);
+
+var l = 114;
+l--;
+console.log(l);
+
+var p = 20;
+var q = p**2;
+console.log(q);
+
+var x = 15;
+x+=15;
+console.log(x);
+
+var r = 15;
+r-=15;
+console.log(r);
+
+var m = 10;
+m*=10;
+console.log(m);
+
+var n = 10;
+n/=10;
+console.log(n);
+
+var v = 5;
+var w = 5;
+console.log(v == w);
+
+var s = 5;
+var t = 6;
+console.log(s != t);
+
+var num1 = 100;
+var num2 = 10;
+console.log(num1 > num2);
+
+var num3 = 2;
+var num4 = 1;
+console.log(num3 < num4);
+
+var num5 = 123;
+var num6 = "123";
+console.log(num5 === num6);
+
+var firstName = "Digital";
+var lastName = "School";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var school = "Digital School";
+console.log(`Hello from ${school}`);
+
+var age = 23;
+console.log(age >= 15 && age <=65);
+
+var var1 = 5;
+var var2 = 6;
+console.log(var1 == 5 || var2 == 5);
+
+
