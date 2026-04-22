9e467a10f056e6fbd8c962db6405139cfd293711
Festinna <festinakrasniqi09@gmail.com>
2026-04-22 15:58:04 +0200

    lesson28

---
+++ lesson29/main.js
@@ -0,0 +1,55 @@
+var programmingLanguages = ['JavaScript', 'PHP', 'Python'];
+
+console.log(programmingLanguages);
+
+console.log(programmingLanguages[0]);
+
+
+programmingLanguages.push('Java');
+console.log(programmingLanguages);
+
+
+programmingLanguages.pop();
+console.log(programmingLanguages);
+
+
+programmingLanguages.unshift('c#');
+console.log(programmingLanguages);
+
+
+programmingLanguages.shift();
+console.log(programmingLanguages);
+
+programmingLanguages.splice(0,2,'Ruby');
+console.log(programmingLanguages);
+
+console.log (Math.random()*5);
+
+console.log (Math.floor(Math.random()*5));
+
+var students = ['Festina', 'Anisa'];
+
+var [s1, s2] = students;
+
+console.log(s1);
+console.log(s2);
+
+var places = ["London", "Paris", "New York", "Berlin"];
+
+var [firstPlace, secondPlace] = places;
+
+console.log(secondPlace);
+
+
+
+var number = [1,2,3,4,5,6,7,8,9,10];
+
+var[first, second, ...otherNumbers] = numbers;
+
+console.log(first);
+console.log(second);
+console.log(otherNumbers.toString());
+
+
+
+
