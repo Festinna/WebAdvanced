fecfaa69a523d5d7e1d76e162c8617700b89fcd1
Festinna <festinakrasniqi09@gmail.com>
2026-04-22 16:08:59 +0200

    comit

---
+++ lesson29/easychallange/main.js
@@ -0,0 +1,10 @@
+var students = ['Anisa,' 'Mjellma', 'Rita', 'Valeza', 'Diella'];
+
+console.log(students);
+
+var[s1,s2,s3,s4,s5,];
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+console.log(s5);
