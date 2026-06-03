b26017e8d6771dccd865fc9b39f44f79eb7c9120
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-06-03 16:12:36 +0200

    36

---
+++ lesson36/part1/main.js
@@ -0,0 +1,17 @@
+$(window).ready(function(){
+    alert('Is it working!!!')
+});
+
+$('#header');
+$('li');
+$('ul li');
+$('.student');
+
+$('a.test:first');
+$('tr:odd');
+$('#myForm :input');
+$('div:visible');
+$('div:gt(2)');
+$('div:animated');
+
+
